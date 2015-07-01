33ed7cabae082fdfce0e7cca5d4d16284a21a1f6
Shriya Nevatia <sneva831@gmail.com>
2015-07-01 15:33:18 -0400

    part 8 of makestagram part 1

---
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TimelineViewController.swift
+//  Makestagram
+//
+//  Created by Shriya Nevatia on 7/1/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBarController?.delegate = self
+    }
+    
+}
+
+// MARK: Tab Bar Delegate
+
+extension TimelineViewController: UITabBarControllerDelegate {
+    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if (viewController is PhotoViewController) {
+            println("Take Photo")
+            return false
+        } else {
+            return true
+        }
+    }
+    
+}
+
